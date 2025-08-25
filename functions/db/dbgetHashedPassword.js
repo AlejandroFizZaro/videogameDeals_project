9d83fade7f0bffe51739d5811eb33ee307862d7d
Alex <alejandrofizzaro@gmail.com>
2025-08-25 22:50:20 +0200

    User table now insert hashed passwords, sessions table insert coded JWT and the backend can validate incoming JWT

--- functions/db/dbgetHashedPassword.js
+++
@@ -1,8 +0,0 @@
-import sql from "./dbConnect.js";
-
-let getHashedPasswordfromDb = async (id) => {
-	let hashPassword = await sql`SELECT password from users where id = ${id}`;
-	return hashPassword.password;
-};
-
-export default getHashedPasswordfromDb;
